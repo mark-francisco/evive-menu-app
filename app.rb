8acac9afdcd8a2549c1c14ce58139cd173967ef1
Mark Francisco <mfrancisco581@gmail.com>
2021-07-21 12:59:28 -0500

    return menu items. add todos

--- app.rb
+++ app.rb
@@ -2,7 +2,6 @@ puts "Welcome to the Menu Ordering System."
 puts "Enter your order in the following format: 'Meal item1,item2,item3'."
 puts "When you are done, input 'exit' to quit the application."
 
-# note to self: consider moving data variables to a different file
 menu_mapping = {
   "1" => "Main",
   "2" => "Side",
@@ -30,15 +29,23 @@ while true
     key == meal
   }
 
+  current_menu = current_menu.values[0]
+
+  menu_items.sort!
   menu_items.map! { |menu_item|
     menu_item = menu_mapping[menu_item]
   }
 
-  pp menu_items
+  # pp menu_items
+  # pp current_menu
 
-  # menu_items.each { |menu_item|
-  #   pp current_menu.select { |menu_course|
-  #     menu_course == menu_mapping[menu_item]
-  #   }
-  # }
+  menu_items.each { |menu_item|
+    pp current_menu[menu_item]
+  }
 end
+
+# TODO: object-oriented organization
+# TODO: handling when multiple items are ordered
+# TODO: validations for all meals
+# TODO: validations based on breakfast, lunch, dinner
+# TODO: Minitest unit tests
