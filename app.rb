252eeec81e9bbe0b7939e7541f19304670e1f78d
Mark Francisco <mfrancisco581@gmail.com>
2021-07-21 12:33:05 -0500

    add functionality to parse user input into menu items

--- app.rb
+++ app.rb
@@ -3,30 +3,17 @@ puts "Enter your order in the following format: 'Meal item1,item2,item3'."
 puts "When you are done, input 'exit' to quit the application."
 
 # note to self: consider moving data variables to a different file
-menu_mapping = [
-  { 1 => :Main },
-  { 2 => :Side },
-  { 3 => :Drink },
-  { 4 => :Dessert },
-]
-menus = [
-  { :Breakfast => [
-    { :Main => "Eggs" },
-    { :Side => "Toast" },
-    { :Drink => "Coffee" },
-  ] },
-  { :Lunch => [
-    { :Main => "Sandwich" },
-    { :Side => "Chips" },
-    { :Drink => "Soda" },
-  ] },
-  { :Dinner => [
-    { :Main => "Steak" },
-    { :Side => "Potatoes" },
-    { :Drink => "Wine" },
-    { :Dessert => "Cake" },
-  ] },
-]
+menu_mapping = {
+  "1" => "Main",
+  "2" => "Side",
+  "3" => "Drink",
+  "4" => "Dessert",
+}
+menus = {
+  "Breakfast" => { "Main" => "Eggs", "Side" => "Toast", "Drink" => "Coffee" },
+  "Lunch" => { "Main" => "Sandwich", "Side" => "Chips", "Drink" => "Soda" },
+  "Dinner" => { "Main" => "Steak", "Side" => "Potatoes", "Drink" => "Wine", "Dessert" => "Cake" },
+}
 
 while true
   puts "What's the order?"
@@ -35,4 +22,23 @@ while true
     puts "Thanks for dining with us. Come again soon."
     break
   end
+  order = order.split(" ")
+  meal = order[0].capitalize()
+  menu_items = order[1].split(",")
+
+  current_menu = menus.select { |key, _value|
+    key == meal
+  }
+
+  menu_items.map! { |menu_item|
+    menu_item = menu_mapping[menu_item]
+  }
+
+  pp menu_items
+
+  # menu_items.each { |menu_item|
+  #   pp current_menu.select { |menu_course|
+  #     menu_course == menu_mapping[menu_item]
+  #   }
+  # }
 end
