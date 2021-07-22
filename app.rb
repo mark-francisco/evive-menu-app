3fd91f8cb930657d84d3b0c1374adddd1b76f2bd
Mark Francisco <mfrancisco581@gmail.com>
2021-07-21 23:04:41 -0500

    refactor into OOP

--- app.rb
+++ app.rb
@@ -1,107 +1,62 @@
-puts "Welcome to the Menu Ordering System."
+require_relative "meal.rb"
+
+menu_mapping = {
+  "1" => "Main",
+  "2" => "Side",
+  "3" => "Drink",
+  "4" => "Dessert",
+}
+
 puts "Enter your order in the following format: 'Meal item1,item2,item3'."
 puts "When you are done, input 'exit' to quit the application."
 
 while true
-  menu_mapping = {
-    "1" => "Main",
-    "2" => "Side",
-    "3" => "Drink",
-    "4" => "Dessert",
-  }
-  menus = {
-    "Breakfast" => { "Main" => "Eggs", "Side" => "Toast", "Drink" => "Coffee" },
-    "Lunch" => { "Main" => "Sandwich", "Side" => "Chips", "Drink" => "Soda" },
-    "Dinner" => { "Main" => "Steak", "Side" => "Potatoes", "Drink" => "Wine", "Dessert" => "Cake" },
-  }
-
   puts "What's the order?"
-  order = gets.chomp
-  if order.downcase == "exit"
+  order_input = gets.chomp
+  if order_input.downcase == "exit"
     puts "Thanks for dining with us. Come again soon."
-    break
+    return
   end
 
-  order = order.split(" ")
-  meal = order[0].capitalize()
-  menu_array = order[1].split(",")
+  order_input = order_input.split(" ")
 
-  # Validations
-  if !order[1].include?("1")
-    puts "Unable to process: Main is missing"
-    # return
-  elsif !order[1].include?("2")
-    puts "Unable to process: Side is missing"
-    # return
+  if order_input[0].capitalize() == "Breakfast"
+    meal = Breakfast.new
+  elsif order_input[0].capitalize() == "Lunch"
+    meal = Lunch.new
+  elsif order_input[0].capitalize() == "Dinner"
+    meal = Dinner.new
+  else
+    puts "Sorry, that's not a valid meal."
+    return
   end
 
-  current_menu = menus.select { |key, _value|
-    key == meal
-  }.values[0]
+  menu_array = order_input[1].split(",")
 
-  if !order[1].include?("3")
-    current_menu["Drink"] = "Water"
-    menu_array.push("3")
-  end
-
-  menu_array.sort!
-  menu_array.map! { |menu_item|
-    menu_item = menu_mapping[menu_item]
+  menu_array.map! { |item|
+    item = menu_mapping[item]
+  }
+  menu_array.each { |menu_course|
+    meal.order[menu_course] += 1
   }
 
-  # pp menu_array
-  # pp current_menu
-
-  order_counts = {}
-
-  menu_array.each { |menu_arr_item|
-    if order_counts[current_menu[menu_arr_item]]
-      order_counts[current_menu[menu_arr_item]] += 1
-    else
-      order_counts[current_menu[menu_arr_item]] = 1
-    end
-  }
-
-  pp menu_array
-
-  if meal == "Breakfast" && menu_array.count("Main") > 1
-    puts "#{current_menu["Main"]} cannot be ordered more than once"
-    # return
-  end
-  if meal == "Breakfast" && menu_array.count("Side") > 1
-    puts "#{current_menu["Side"]} cannot be ordered more than once"
-    # return
-  end
-
-  if meal == "Lunch" && menu_array.count("Main") > 1
-    puts "#{current_menu["Main"]} cannot be ordered more than once"
-    # return
-  end
-  if meal == "Lunch" && menu_array.count("Drink") > 1
-    puts "#{current_menu["Drink"]} cannot be ordered more than once"
-    # return
-  end
-
-  if meal == "Dinner" && menu_array.count("Dessert") < 1
-    puts "Dessert is missing"
-    # return
-  end
-  if meal == "Dinner"
-    order_counts["Water"] = 1
-    # return
+  meal.validate_meal_rules
+  meal.convert_to_menu_items
+  if meal.is_a?(Breakfast)
+    meal.validate_breakfast_rules
+  elsif meal.is_a?(Lunch)
+    meal.validate_lunch_rules
+  elsif meal.is_a?(Dinner)
+    meal.validate_dinner_rules
   end
 
   final_output = []
-  order_counts.each { |key, value|
+  meal.order.each { |key, value|
     if value == 1
       final_output << key
-    else
+    elsif value > 0
       final_output << "#{key}(#{value})"
     end
   }
   pp final_output.join(", ")
 end
-
-# TODO: object-oriented organization
-# TODO: validations based on breakfast, lunch, dinner
-# TODO: Minitest unit tests
--- meal.rb
+++ meal.rb
@@ -1,41 +1,115 @@
 class Meal
-  def initialize
-    @menu_mapping = {
-      "1" => "Main",
-      "2" => "Side",
-      "3" => "Drink",
-      "4" => "Dessert",
-    }
+  def validate_meal_rules
+    if @order["Main"] < 1
+      raise StandardError.new("Unable to proces: Main is missing")
+    end
+    if @order["Side"] < 1
+      raise StandardError.new("Unable to proces: Side is missing")
+    end
   end
 end
 
 class Breakfast < Meal
-  attr_accessor :main, :side, :drink
+  attr_accessor :order
 
-  def initialize(input_hash = {})
-    @main = "Eggs"
-    @side = "Toast"
-    @drink = "Coffee" || "Water"
+  def initialize
+    @order = {
+      "Main" => 0,
+      "Side" => 0,
+      "Drink" => 0,
+    }
+  end
+
+  def validate_meal_rules
+    super
+  end
+
+  def convert_to_menu_items
+    @order["Eggs"] = @order.delete("Main")
+    @order["Toast"] = @order.delete("Side")
+    if @order["Drink"] == 0
+      @order.delete("Drink")
+      @order["Water"] = 1
+    else
+      @order["Coffee"] = @order.delete("Drink")
+    end
+  end
+
+  def validate_breakfast_rules
+    if @order["Eggs"] && @order["Eggs"] > 1
+      raise StandardError.new("Unable to process: Eggs cannot be ordered more than once")
+    end
+
+    if @order["Toast"] && @order["Toast"] > 1
+      raise StandardError.new("Unable to process: Toast cannot be ordered more than once")
+    end
   end
 end
 
 class Lunch < Meal
-  attr_accessor :main, :side, :drink
+  attr_accessor :order
 
   def initialize
-    @main = "Sandwich"
-    @side = "Chips"
-    @drink = "Soda"
+    @order = {
+      "Main" => 0,
+      "Side" => 0,
+      "Drink" => 0,
+    }
+  end
+
+  def validate_meal_rules
+    super
+  end
+
+  def convert_to_menu_items
+    @order["Sandwich"] = @order.delete("Main")
+    @order["Chips"] = @order.delete("Side")
+    if @order["Drink"] == 0
+      @order.delete("Drink")
+      @order["Water"] = 1
+    else
+      @order["Soda"] = @order.delete("Drink")
+    end
+  end
+
+  def validate_lunch_rules
+    if @order["Sandwich"] && @order["Sandwich"] > 1
+      raise StandardError.new("Unable to process: Sandwich cannot be ordered more than once")
+    end
+
+    if @order["Soda"] && @order["Soda"] > 1
+      raise StandardError.new("Unable to process: Soda cannot be ordered more than once")
+    end
   end
 end
 
 class Dinner < Meal
-  attr_accessor :main, :side, :drink
+  attr_accessor :order
 
   def initialize
-    @main = "Steak"
-    @side = "Potatoes"
-    @drink = "Wine"
-    @dessert = "Cake"
+    @order = {
+      "Main" => 0,
+      "Side" => 0,
+      "Drink" => 0,
+      "Dessert" => 0,
+    }
+  end
+
+  def validate_meal_rules
+    super
+  end
+
+  def convert_to_menu_items
+    @order["Steak"] = @order.delete("Main")
+    @order["Potatoes"] = @order.delete("Side")
+    @order["Wine"] = @order.delete("Drink")
+    @order["Water"] = 1
+    @order["Cake"] = @order.delete("Dessert")
+  end
+
+  def validate_dinner_rules
+    if @order["Cake"] < 1
+      raise StandardError.new("Unable to process: Dessert is missing")
+    end
   end
 end
